73df63dbb2c620276e075b57fd8769b5402d3a4f
Jordan Tucker <jordanbtucker@gmail.com>
2019-01-15 16:05:10 -0600

    refactor: remove debug

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,6 @@
-import Debug = require('debug')
-
-const debug = Debug('ts-init')
-
 class App {
 	public async run () {
 		const message = await Promise.resolve(`Hello, ${process.env.NAME}!`)
-		debug('message: %s', message)
 		console.log(message)
 	}
 }
