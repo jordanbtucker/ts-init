0f8ac22f5785124cfeac3c6ab6cf38acb168392b
Jordan Tucker <jordanbtucker@gmail.com>
2020-05-23 12:37:24 -0500

    fix: use correct API for sinon

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 class App {
   public async run(): Promise<void> {
-    const message = await Promise.resolve(`Hello, ${process.env.NAME}!`)
+    const message = await Promise.resolve(
+      `Hello, ${process.env.NAME || 'user'}!`,
+    )
     console.log(message)
   }
 }
